b5675024eb64432e5b2b69a309cd89a72fcb78c8
awktane <bmackie@awktane.net>
2026-06-26 12:38:25 -0700

    Add temp_on_network option and improve language handling

--- Community/Tdarr_Plugin_awk_stream_ordering.js
+++ Community/Tdarr_Plugin_awk_stream_ordering.js
@@ -16,6 +16,8 @@ const details = () => ({
             inputUI: { type: 'text' },
             tooltip: `Comma separated language priority list (e.g. eng,jpn,und). Leave blank to leave language order untouched.
                  \\nAny languages not mentioned will be grouped by channel, etc but will be left in the language order they appear in the file.
+                 \\nThis list should include both two character and three character codes as this will successfully catch values like en, eng, en-US, en_US, and en.US.
+                 \\nIf two character is provided in the list then languages formatted like en-US will be treated as en
                  \\nExample: (channel_order ascending and preferred_languages eng,jpn)\\n
                  \\nA file containing ger 2.0,fre 2.0,eng 2.0,jpn 2.0,eng 5.1,jpn 5.1 would be reordered eng 5.1,eng 2.0,jpn 5.1,jpn 2.0,ger 2.0,fre 2.0`,
         },
@@ -67,6 +69,17 @@ const details = () => ({
             },
             tooltip: 'Should we put the default audio first? It is often inaccurate depending on the file source.',
         },
+        {
+            name: 'temp_on_network',
+            type: 'boolean',
+            defaultValue: true,
+            inputUI: {
+                type: 'dropdown',
+                options: ['true', 'false'],
+            },
+            tooltip: `Is the temp folder on the network? Enabling this adds stops ffmpeg from flushing the buffer quite as often. (-flush_packets 0)
+                 \\nGenerally speaking this has very little effect if the files are local instead and therefore it's enabled by default.`,
+        },
     ],
 });
 
@@ -91,6 +104,9 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
     const sdhFirst = String(inputs.sdh_first) === 'true';
     const defaultAudioFirst = String(inputs.default_audio_first) === 'true';
 
+    //This is the only option I found that consistently made a difference. Not a huge difference but nonetheless...
+    const networkDataOpt = (String(inputs.temp_on_network) === 'true' ? ' -flush_packets 0' : '');
+
     // collect the other languages from file
     const languageOrder = new Set(preferredLanguages);
     const streams = [];
@@ -98,9 +114,10 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
         const ffstream = file.ffProbeData.streams[i];
         const streamTitle = (ffstream.tags?.title || '').trim().toLowerCase();
         const streamLang = (ffstream.tags?.language || 'und').trim().toLowerCase();
+        const streamLangShort = streamLang.replace(/[-_.].*$/, '');
 
-        //Add it to the list of languages
-        if (!languageOrder.has(streamLang)) {
+        //Add it to the list of languages if we don't have it or the two letter code that starts it if like en-US
+        if (!languageOrder.has(streamLang) && !languageOrder.has(streamLangShort)) {
             preferredLanguages.push(streamLang);
             languageOrder.add(streamLang);
         }
@@ -111,9 +128,10 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
             type: (ffstream.codec_type || '').trim().toLowerCase(),
             title: streamTitle,
             lang: streamLang,
+            shortlang: streamLangShort,
             channels: ffstream?.channels || 0,
             forced: ffstream?.disposition?.forced === 1,
-            bitrate: Number (ffstream?.bit_rate || 0),
+            bitrate: Number(ffstream?.bit_rate || 0),
             default: ffstream?.disposition?.default === 1,
 
             // simple classification (no helper functions)
@@ -164,8 +182,11 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
                 return a.default ? -1 : 1;
 
             // language priority
-            const aLang = preferredLanguages.indexOf(a.lang);
-            const bLang = preferredLanguages.indexOf(b.lang);
+            let aLang = preferredLanguages.indexOf(a.lang);
+            let bLang = preferredLanguages.indexOf(b.lang);
+
+            if(aLang === -1) aLang = preferredLanguages.indexOf(a.shortlang);
+            if(bLang === -1) bLang = preferredLanguages.indexOf(b.shortlang);
 
             const aRank = aLang === -1 ? 999 : aLang;
             const bRank = bLang === -1 ? 999 : bLang;
@@ -193,8 +214,11 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
             if (a.forced !== b.forced)
                 return a.forced ? -1 : 1;
 
-            const aLang = preferredLanguages.indexOf(a.lang);
-            const bLang = preferredLanguages.indexOf(b.lang);
+            let aLang = preferredLanguages.indexOf(a.lang);
+            let bLang = preferredLanguages.indexOf(b.lang);
+
+            if(aLang === -1) aLang = preferredLanguages.indexOf(a.shortlang);
+            if(bLang === -1) bLang = preferredLanguages.indexOf(b.shortlang);
 
             const aRank = aLang === -1 ? 999 : aLang;
             const bRank = bLang === -1 ? 999 : bLang;
@@ -225,7 +249,7 @@ const plugin = (file, librarySettings, inputs, otherArguments) => {
 
     response.processFile = true;
     response.reQueueAfter = true;
-    response.preset = `,${ffmpegMap} -c copy -max_muxing_queue_size 9999`;
+    response.preset = `,${ffmpegMap} -c copy -max_muxing_queue_size 9999${networkDataOpt}`;
     response.infoLog += '☒ Reordered streams (smart ordering applied)\n';
 
     return response;
